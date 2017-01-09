2f9cf8a3cb71151dd9d7958ebea25385c05e5bf5
Dan Zaratsian <dtz001@gmail.com>
2017-01-09 12:59:13 -0500

    Update start_kafka.sh

--- containers/kafka/assets/start_kafka.sh
+++ containers/kafka/assets/start_kafka.sh
@@ -1,4 +1,22 @@
 source /root/.bashrc
 
+echo ""
+echo ""
+echo "Starting Kafka..."
 ./kafka/bin/zookeeper-server-start.sh /kafka/config/zookeeper.properties &
-./kafka/bin/kafka-server-start.sh /kafka/config/server.properties
+./kafka/bin/kafka-server-start.sh /kafka/config/server.properties &
+
+echo ""
+echo ""
+echo "Creating Kafka topic..."
+/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic dztopic1
+/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --list
+echo ""
+echo ""
+echo "Installing python kafka and pip..."
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+pip install kafka
+echo ""
+echo ""
+echo "Startup Complete!"
