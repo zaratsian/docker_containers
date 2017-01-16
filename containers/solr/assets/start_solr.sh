10526cbbe2d46866f09d5c34fa2824e8c75a5693
Dan Zaratsian <dtz001@gmail.com>
2017-01-16 09:45:36 -0500

    Update start_solr.sh

--- containers/solr/assets/start_solr.sh
+++ containers/solr/assets/start_solr.sh
@@ -1,9 +1,10 @@
 echo ""
 echo ""
-echo "Starting Solr on Port 8983..."
-/solr/bin/solr start -p 8983
+echo "Starting Solr in Cloud mode on Port 8983 (and trying to connect to zookeeper port 2181)..."
+/solr/bin/solr start -c -z localhost:2181 -p 8983
 sleep 10
 echo ""
 echo ""
-echo "Solr start on Port 8983!"
+echo "SolrCloud start on Port 8983!"
+echo "If failed, check to make sure zookeeper is running on port 2181"
 echo ""
