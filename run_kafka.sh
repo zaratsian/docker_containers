739beceaef9c048713da1f69eea42dfb7a320315
Dan Zaratsian <dtz001@gmail.com>
2017-02-27 09:16:58 -0500

    Update run_kafka.sh

--- run_kafka.sh
+++ run_kafka.sh
@@ -5,11 +5,11 @@ docker rm -f kafka
 docker run -it -d --hostname kafka --net dev --name kafka kafka
 
 docker cp /containers/kafka/assets/stream_kafka.py kafka:/stream_kafka.py
-docker exec kafka /kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic dztopic1
-docker exec kafka /kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --list
-docker exec kafka curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-docker exec kafka python get-pip.py
-docker exec kafka pip install kafka
+#docker exec kafka /kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic dztopic1
+#docker exec kafka /kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --list
+#docker exec kafka curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+#docker exec kafka python get-pip.py
+#docker exec kafka pip install kafka
 
 
 echo "*****************************************************"
