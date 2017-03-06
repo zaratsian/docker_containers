2277bb35ff46b1b47533e1d808c0971dd0010a9a
Dan Zaratsian <dtz001@gmail.com>
2017-03-06 09:31:36 -0500

    Added Spark 2.1.0

--- stop_all.sh
+++ stop_all.sh
@@ -1,3 +1,3 @@
 
-docker stop phoenix spark_162 spark_202 nifi kafka storm solr
-docker rm -f phoenix spark_162 spark_202 nifi kafka storm solr
+docker stop phoenix spark_162 spark_202 spark_210 nifi kafka storm solr
+docker rm -f phoenix spark_162 spark_202 spark_210 nifi kafka storm solr
