7ee31cbffa61729ede91716d7742e066a39c2dbd
Dan Zaratsian <dtz001@gmail.com>
2017-03-21 19:15:50 -0400

    Update run_spark_202.sh

--- run_spark_202.sh
+++ run_spark_202.sh
@@ -5,8 +5,8 @@ docker rm -f spark_162 spark_202
 docker run -it -d -p 8079:8080 -p 4040:4040 --hostname spark_202 --net dev --name spark_202 spark_202
 
 # Install pip
-#docker exec spark_202 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-#docker exec spark_202 python get-pip.py
+docker exec spark_202 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+docker exec spark_202 python get-pip.py
 
 
 echo "*****************************************************"
