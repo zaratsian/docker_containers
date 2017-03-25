625860877871c662c6fff9168386c33359d19c50
Dan Zaratsian <dtz001@gmail.com>
2017-03-25 12:29:24 -0400

    Update start_nifi.sh

--- containers/nifi/assets/start_nifi.sh
+++ containers/nifi/assets/start_nifi.sh
@@ -1,9 +1,13 @@
 echo ""
 echo ""
-echo "Starting NiFi"
+echo "[ INFO ] Setting port 1985 as the NiFi port..."
+sed -i 's/8080/1985/g' /nifi/conf/nifi.properties 
+echo ""
+echo "[ INFO ] Starting NiFi..."
 /nifi/bin/nifi.sh start
+echo ""
 #tail -f /nifi/logs/nifi-app.log
 echo ""
 echo ""
-echo "NiFi initiated, should start up in a minute on port 1985..."
+echo "NiFi started on Port 1985"
 echo ""
