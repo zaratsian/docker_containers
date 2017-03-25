78f33c868d5c3b0609f3fc0b2dac002d418f2eb8
Dan Zaratsian <dtz001@gmail.com>
2017-03-25 12:28:50 -0400

    Update run_nifi.sh

--- run_nifi.sh
+++ run_nifi.sh
@@ -4,13 +4,10 @@ docker rm -f nifi
 
 docker run -it -d -p 8080:8080 --hostname nifi --net dev --name nifi nifi
 
-# Change NiFi Port number (default is 8080), but I change it to 1985 here. 
-sed -i 's/8080/1985/g' /nifi/conf/nifi.properties 
-
 
 echo "*****************************************************"
 echo "*"
-echo "*  Spark Container has been started..."
+echo "*  NiFi Container has been started..."
 echo "*"
 echo "*  Port (NiFi):     1985"
 echo "*  Port (Zeppelin): 8079"
