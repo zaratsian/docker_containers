4251e14b4c72720ecd191831085916a8502adea5
Dan Zaratsian <dtz001@gmail.com>
2017-05-30 12:32:35 -0400

    Update run_solr.sh

--- run_solr.sh
+++ run_solr.sh
@@ -9,8 +9,6 @@ docker exec solr git clone https://github.com/lucidworks/banana.git
 docker exec solr cp -r /banana /solr/server/solr-webapp/webapp/banana
 # Browse to http://localhost:8983/solr/banana/src/index.html
 
-# Solr-Twitter Demo setup
-
 # Install pip
 docker exec solr curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 docker exec solr python get-pip.py
@@ -21,6 +19,13 @@ docker exec solr tar -xzvf /kafka.tgz
 docker exec solr mv kafka_2.10-0.10.1.1 /kafka
 docker cp containers/kafka/assets/start_kafka.sh solr:/.
 docker cp containers/kafka/assets/stream_kafka.py solr:/.
+
+# Solr-Twitter Demo setup
+docker exec solr cp -r /solr/server/solr/configsets/data_driven_schema_configs /solr/server/solr/configsets/tweet_configs
+docker cp containers/solr/assets/solrconfig.xml solr:/solr/server/solr/configsets/tweet_configs/conf/solrconfig.xml
+docker exec solr mv /solr/server/solr-webapp/webapp/banana/src/app/dashboards/default.json /solr/server/solr-webapp/webapp/banana/src/app/dashboards/default.json.orig
+docker exec solr wget https://raw.githubusercontent.com/hortonworks/data-tutorials/master/tutorials/hdp/hdp-2.5/analyzing-social-media-and-customer-sentiment-with-apache-nifi-and-hdp-search/assets/default.json
+docker exec solr cp /default.json /solr/server/solr-webapp/webapp/banana/src/app/dashboards/default.json
 
 
 echo "****************************************************************"
