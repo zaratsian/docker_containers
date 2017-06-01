16a20f069024035105774ecfb9e0dfa256a1f52d
Dan Zaratsian <dtz001@gmail.com>
2017-06-01 18:58:18 -0400

    Update run_nifi.sh

--- run_nifi.sh
+++ run_nifi.sh
@@ -2,17 +2,19 @@
 docker stop nifi
 docker rm -f nifi
 
-docker run -it -d -p 1985:1985 --hostname nifi --net dev --name nifi nifi
+docker run -it -d -p 1985:1985 -p 19999:19999 --hostname nifi --net dev --name nifi nifi
 
+# Port 19999 is used as reserve port
+
+# Copy NiFi Assets
 docker cp containers/nifi/assets/start_nifi.sh nifi:/.
+
 
 echo "*****************************************************"
 echo "*"
 echo "*  NiFi Container has been started..."
 echo "*"
 echo "*  Port (NiFi):     1985"
-echo "*  Port (Zeppelin): 8079"
-echo "*  Port (Phoenix):  8765"
 echo "*"
 echo "*  Usage: docker exec -it nifi bash"
 echo "*"
