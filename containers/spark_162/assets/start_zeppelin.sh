a61c2951758a51cd7ad9ae81e60bd7fc1de0a594
Dan Zaratsian <dtz001@gmail.com>
2017-08-13 11:46:40 -0400

    Update start_zeppelin.sh

--- containers/spark_162/assets/start_zeppelin.sh
+++ containers/spark_162/assets/start_zeppelin.sh
@@ -1,1 +1,7 @@
+print ""
+print "###############################################"
+print ""
+print "[ INFO ] Starting Zeppelin on Port 8080..."
+print ""
+print "###############################################"
 /zeppelin/bin/zeppelin.sh run
