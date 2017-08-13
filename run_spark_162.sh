a0b86f633fd87ba6779b99ce4479282afe89c047
Dan Zaratsian <dtz001@gmail.com>
2017-08-12 22:59:27 -0400

    Update run_spark_162.sh

--- run_spark_162.sh
+++ run_spark_162.sh
@@ -4,12 +4,12 @@ docker rm -f spark_162 spark_202
 
 docker run -it -d -p 8079:8080 -p 4040:4040 --hostname spark_162 --net dev --name spark_162 spark_162
 
-docker cp containers/spark_162/assets/log4j.properties spark_162:/spark/conf/log4j.properties
-docker exec spark_162 wget https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-docker exec spark_162 tar xvf apache-maven-3.3.9-bin.tar.gz
-docker exec spark_162 wget http://central.maven.org/maven2/org/apache/phoenix/phoenix-spark/4.8.1-HBase-1.1/phoenix-spark-4.8.1-HBase-1.1.jar
-docker exec spark_162 rm -rf /zeppelin/notebook
-docker exec spark_162 cp -rf /assets/notebook/ /zeppelin/notebook/
+#docker cp containers/spark_162/assets/log4j.properties spark_162:/spark/conf/log4j.properties
+#docker exec spark_162 wget https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+#docker exec spark_162 tar xvf apache-maven-3.3.9-bin.tar.gz
+#docker exec spark_162 wget http://central.maven.org/maven2/org/apache/phoenix/phoenix-spark/4.8.1-HBase-1.1/phoenix-spark-4.8.1-HBase-1.1.jar
+#docker exec spark_162 rm -rf /zeppelin/notebook
+#docker exec spark_162 cp -rf /assets/notebook/ /zeppelin/notebook/
 
 
 echo "*****************************************************"
@@ -23,4 +23,8 @@ echo "*"
 echo "*  Usage: docker exec -it spark_162 bash"
 echo "*"
 echo "*****************************************************"
+echo ""
+echo "Manually install knitr for SparkR"
+echo ""
+echo ""
 
