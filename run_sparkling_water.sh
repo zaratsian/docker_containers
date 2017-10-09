c1a7f1456a72d09e9582293b5b2954d5e8d46f23
Dan Zaratsian <dtz001@gmail.com>
2017-10-08 21:54:43 -0400

    Updated Sparkling Water

--- run_sparkling_water.sh
+++ run_sparkling_water.sh
@@ -1,17 +1,20 @@
 
-docker run -it -d -p 18088:8088 -p 14040:4040 -p 14444:4444 -p 12181:2181 --hostname sparkling_water --net dev --name sparkling_water sparkling_water
+docker run -it -d -p 18088:8088 -p 19090:8080 -p 14444:4444 -p 15555:5555 --hostname sparkling_water --net dev --name sparkling_water sparkling_water
 
 echo ""
 echo "Enter password for Apache Superset (username=admin):"
 sleep 5
 docker exec sparkling_water /bin/sh -c 'fabmanager create-admin --app superset < /admin.config'
 
+docker exec sparkling_water superset runserver &
+
+
 echo "*****************************************************"
 echo "*"
 echo "*  Container has been started..."
 echo "*"
 echo "*  Port (Superset):  18088"
-echo "*  Port (Zookeeper): 12181"
+echo "*  Port (Zeppelin):  19090"
 echo "*"
 echo "*  Usage: docker exec -it sparkling_water bash"
 echo "*"
