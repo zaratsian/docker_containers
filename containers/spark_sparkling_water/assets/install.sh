1bb7fdefa102196a24ffd81f149811b16c193e25
Dan Zaratsian <dtz001@gmail.com>
2017-10-20 21:18:51 -0400

    Update install.sh

--- containers/spark_sparkling_water/assets/install.sh
+++ containers/spark_sparkling_water/assets/install.sh
@@ -1,11 +1,21 @@
 
-export JAVA_HOME=/usr/lib/jvm/java
-export SPARK_HOME=/spark
-export PYTHONPATH=/sparkling-water-2.1.14/py/build/dist/h2o_pysparkling_2.1-2.1.14.zip:$PYTHONPATH
-export PYSPARK_PYTHON=/opt/conda/bin/python2.7
+echo "[ INFO ] Environment Variables"
+echo "" >> ~/.bashrc
+#echo "export JAVA_HOME=/usr/lib/jvm/java" >> ~/.bashrc
+echo "export SPARK_HOME=/spark" >> ~/.bashrc
+echo "export PYTHONPATH=/sparkling-water-2.1.14/py/build/dist/h2o_pysparkling_2.1-2.1.14.zip:$PYTHONPATH" >> ~/.bashrc
+echo "export PYSPARK_PYTHON=/opt/conda/bin/python2.7" >> ~/.bashrc
 
+
+echo "[ INFO ] Install Python Packages"
 pip install tabulate
 pip install future
 
+
 # Start Livy Server
-/livy/bin/livy-server &
+echo "[ INFO ] Starting Livy Server"
+export SPARK_HOME=/spark
+nohup /livy/bin/livy-server &
+
+
+#ZEND
