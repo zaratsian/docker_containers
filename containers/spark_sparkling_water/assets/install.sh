094743742d082bac196369b46ba2260c464d3874
Dan Zaratsian <dtz001@gmail.com>
2017-10-20 21:47:15 -0400

    Update install.sh

--- containers/spark_sparkling_water/assets/install.sh
+++ containers/spark_sparkling_water/assets/install.sh
@@ -3,7 +3,7 @@ echo "[ INFO ] Environment Variables"
 echo "" >> ~/.bashrc
 #echo "export JAVA_HOME=/usr/lib/jvm/java" >> ~/.bashrc
 echo "export SPARK_HOME=/spark" >> ~/.bashrc
-echo "export PYTHONPATH=/sparkling-water-2.1.14/py/build/dist/h2o_pysparkling_2.1-2.1.14.zip:$PYTHONPATH" >> ~/.bashrc
+echo "export PYTHONPATH=/sparkling-water-2.1.14/py/build/dist/h2o_pysparkling_2.1-2.1.14.zip:\$PYTHONPATH" >> ~/.bashrc
 echo "export PYSPARK_PYTHON=/opt/conda/bin/python2.7" >> ~/.bashrc
 
 
