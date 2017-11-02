67935da7a371084d4fd686b3d90a7bcc2bc87685
Dan Zaratsian <dtz001@gmail.com>
2017-11-02 09:13:10 -0400

    Updated Build

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ docker build -t base containers/base
 docker build -t spark_162 containers/spark_162
 docker build -t spark_202 containers/spark_202
 docker build -t spark_210 containers/spark_210
+docker build -t spark_220 containers/spark_220
 docker build -t kafka containers/kafka
 docker build -t nifi containers/nifi
 docker build -t phoenix containers/phoenix
