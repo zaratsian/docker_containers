14d732350b718ad18502c41224ab274df92703dc
Dan Zaratsian <dtz001@gmail.com>
2017-11-12 17:59:33 -0500

    Update run_spark_220.sh

--- run_spark_220.sh
+++ run_spark_220.sh
@@ -24,6 +24,7 @@ docker exec spark_220 mv kafka_2.10-0.10.1.1 /kafka
 #docker exec spark_220 pip install requests
 
 # Install R (then, manually install knitr)
+docker exec spark_220 yum install -y epel-release
 docker exec spark_220 yum install -y R R-devel libcurl-devel openssl-devel
 
 # Copy Assets
