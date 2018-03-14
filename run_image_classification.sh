8cf699da91707bfdb1a42873086b0e7803261638
Dan Zaratsian <dtz001@gmail.com>
2018-03-14 11:59:06 -0400

    Update run_image_classification.sh

--- run_image_classification.sh
+++ run_image_classification.sh
@@ -4,6 +4,16 @@ docker rm -f image_classification
 docker run -it -d -p 18080:8080 -p 14040:4040 -p 14444:4444 -p 15555:5555 --hostname image_classification --net dev --name image_classification image_classification
 
 echo ""
+echo "Copying Assets Directory..."
+sleep 2
+docker cp containers/image_classification/assets image_classification:/.
+
+echo ""
+echo "Startin App on port 4444 (external port 14444)..."
+sleep 2
+docker exec image_classification /assets/start_app.sh
+
+echo ""
 echo ""
 echo "*****************************************************"
 echo "*"
