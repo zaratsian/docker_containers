4e5d03cb20b47cc7a8e7db02e3e17bc506f7185b
Dan Zaratsian <dtz001@gmail.com>
2018-05-18 14:52:14 -0400

    Update start_solr.sh

--- containers/solr/assets/start_solr.sh
+++ containers/solr/assets/start_solr.sh
@@ -16,12 +16,15 @@ echo ""
 echo ""
 echo "Creating Tweets Solr collection for Solr-Twitter Demo..."
 sleep 2
-/solr/bin/solr create -c tweets -d tweet_configs -s 1 -rf 1 -p 8983
+/solr/bin/solr start -c -z localhost:2181 -p 8983 -force  # -force to run as solr as root (non-secure) 
+#/solr/bin/solr create -c tweets -d tweet_configs -s 1 -rf 1 -p 8983
 sleep 10
+echo "Creating Collection called 'zcollection'"
+/solr/bin/solr create -c zcollection -d data_driven_schema_configs -s 1 -rf 1 -p 8983 -force  # -force to run as solr as root (non-secure) 
 echo ""
 echo ""
-echo "SolrCloud started on Port 8983 (external port 18983)"
-echo "Zookeeper started on Port 2181 (external port 12181)"
+echo "SolrCloud started on Port 8983"
+echo "Zookeeper started on Port 2181"
 echo ""
 echo "If an error is encountered, check to make sure zookeeper is running on port 2181"
 echo ""
